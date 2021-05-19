656c7afb685235d04d75d350eef2ecf38dd0da4c
Shilohjones194 <76697212+Shilohjones194@users.noreply.github.com>
2021-05-19 07:49:46 -0700

    rearranged Test folder/Lib folder and set frameworkinIndex.js

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 // run it with node index.js
 
 //for package.json: "scripts":{"start": "node index.js"}
+
+const fs = require('fs')
+const inquirer = require('inquirer');
+
+const Employee = require('./lib/Employee');
+const Engineer = require('./lib/Engineer');
+const Intern = require('/lib/Intern');
+const Manager = require('/lib/Manager');
+
+const generatePage = require('.')
+const writeHTML = require()
