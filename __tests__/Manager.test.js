1d48e25901893801ccb9b6cf2b6605068ed1c2e9
Shilohjones194 <76697212+Shilohjones194@users.noreply.github.com>
2021-05-21 13:15:42 -0700

    test for video

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,12 +1,12 @@
 const Manager = require('../lib/Manager');
 
 // // to fail test:
-// const testManager = new Manager("l", "lj@gmail.com", 4, "manager", 123-456-7890);
+//const testManager = new Manager("l", "lj@gmail.com", 4, "manager", 123-456-7890);
 
 
 
 //to pass test:
-// const testManager = new Manager("Luke", "shilohjonezz@gmail.com", 4, 123-456-8790);
+const testManager = new Manager("Luke", "shilohjonezz@gmail.com", 4, 123-456-8790);
 
 test('has a name', () => {
     expect(testManager.name).toEqual(expect.any(String))
